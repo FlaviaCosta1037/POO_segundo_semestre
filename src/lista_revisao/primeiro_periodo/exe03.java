30bd8bf3a867e0f537957fa0c778184df19a7222
Flavia Costa <flavinha.goncalves@gmail.com>
2022-02-22 20:57:39 -0300

    "Exercicio 3 = vetor, aplicando método para retornar o indice com o maior valor"

--- src/lista_revisao/primeiro_periodo/exe03.java
+++ src/lista_revisao/primeiro_periodo/exe03.java
@@ -1,6 +1,7 @@
 package lista_revisao.primeiro_periodo;
 //Escreva um programa que receba do usuário um array de
 // números e devolva a posição onde se encontra o maior valor do array.
+//Usando modularização.
 
 import java.util.Arrays;
 import java.util.Locale;
@@ -9,27 +10,38 @@ import java.util.Scanner;
 public class exe03 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner in = new Scanner(System.in);
 
+
+        int vetNumeros[] = new int[3];
+
+        int maiorIndex = indexM(vetNumeros);
+        mostrarResultado(maiorIndex);
+
+    }
+
+    public static int indexM(int maior[]) {
+        Scanner in = new Scanner(System.in);
+        int maiorN = Integer.MIN_VALUE;
         int numero;
-        int maior = 0;
         int index = 0;
         int vetNumeros[] = new int[3];
 
-
         for (int i = 0; i < vetNumeros.length; i++) {
+
             System.out.println("Informe um valor: ");
             numero = in.nextInt();
             vetNumeros[i] = numero;
 
-            if (vetNumeros[i] > maior) {
-                maior = vetNumeros[i];
+            if (vetNumeros[i] > maiorN) {
+                maiorN = vetNumeros[i];
                 index = i;
-
             }
-
         }
-        System.out.println("Posição do maior vetor: " + index);
-        System.out.println("O maior valor é : " + maior);
+        return index;
     }
+
+    public static void mostrarResultado(int value) {
+        System.out.println("O indice com maior valor é: " + value);
+    }
+
 }
