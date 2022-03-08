29ecde5d1436addc976e924aa70ae1f0d1ace0b9
FlaviaCo <flavinha.goncalves@gmail.com>
2022-03-08 16:50:49 -0300

    "Cadastro"

--- src/lista02_classesObjetos/cadastro.java
+++ src/lista02_classesObjetos/cadastro.java
@@ -1,4 +1,4 @@
-/*package lista02_classesObjetos;
+package lista02_classesObjetos;
 
 import lista02_classesObjetos.entidades.Estudante;
 import java.util.Locale;
@@ -10,8 +10,17 @@ public class cadastro {
         Estudante estudante = new Estudante();
         Scanner in = new Scanner(System.in);
 
-        String nome[] = {};
+        String cadastroAlunos[] = new String[3];
+        for(int i = 0; i < cadastroAlunos.length; i++){
+        }
+        System.out.println("Grupo de alunos");
+        System.out.println("----------------");
+        estudante.cadastroAlunos(estudante.nome);
+
+        System.out.println("Informe o nome de um aluno para editar: ");
+        String aluno = in.nextLine();
+
         estudante.editarAlunos();
+
     }
 }
-*/
