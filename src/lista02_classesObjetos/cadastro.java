5677dbf71dcaeb4032cad575dba1923cdcc02e54
FlaviaCo <flavinha.goncalves@gmail.com>
2022-03-10 16:56:10 -0300

    "Produzindo"

--- src/lista02_classesObjetos/cadastro.java
+++ src/lista02_classesObjetos/cadastro.java
@@ -1,26 +1,50 @@
 package lista02_classesObjetos;
 
 import lista02_classesObjetos.entidades.Estudante;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class cadastro {
-    public static void main(String[]args){
+    public static void main(String[]args) {
         Locale.setDefault(Locale.US);
-        Estudante estudante = new Estudante();
-        Scanner in = new Scanner(System.in);
+                Scanner in = new Scanner(System.in);
 
-        String cadastroAlunos[] = new String[3];
-        for(int i = 0; i < cadastroAlunos.length; i++){
+                Estudante estudante = new Estudante();
+
+                System.out.println("Grupo de alunos");
+                System.out.println("----------------");
+
+                estudante.cadEstudantes(estudante.nome);
+
+                System.out.println("Informe o nome de um aluno para editar: ");
+                String aluno = in.nextLine();
+
+
+            }
         }
-        System.out.println("Grupo de alunos");
-        System.out.println("----------------");
-        estudante.cadastroAlunos(estudante.nome);
 
-        System.out.println("Informe o nome de um aluno para editar: ");
-        String aluno = in.nextLine();
 
-        estudante.editarAlunos();
+        /*System.out.printf("Alunos:\n========\n");
+        for (int i = 0; i < estudante.length; i++) {
+            System.out.printf("(%d) %S\n", i + 1, estudante[i].nome);
+        }
+        System.out.printf("Informe o aluno que será editado: ");
+        int selecione = in.nextInt();
 
-    }
-}
+        if(selecione == 1){
+            System.out.printf(" Nome: %s\n Idade: %d\n Curso: %s\n Matricula: %d\n ",estudante[0].nome, estudante[0].idade,
+                    estudante[0].curso, estudante[0].matricula);
+
+        } else if(selecione == 2){
+            System.out.printf(" Nome: %s\n Idade: %d\n Curso: %s\n Matricula: %d\n ",estudante[1].nome, estudante[1].idade,
+                    estudante[1].curso, estudante[1].matricula);
+
+        }else if(selecione == 3){
+            System.out.printf(" Nome: %s\n Idade: %d\n Curso: %s\n Matricula: %d\n ",estudante[2].nome, estudante[2].idade,
+                    estudante[2].curso, estudante[2].matricula);
+
+        }*/
+
+
+
--- src/lista02_classesObjetos/entidades/Estudante.java
+++ src/lista02_classesObjetos/entidades/Estudante.java
@@ -1,18 +1,76 @@
 package lista02_classesObjetos.entidades;
 
-public class Estudante {
-    public String nome[] = {"Flavia", "Maria", "José"};
-    public String idade[] = {"35", "40", "20"};
-    public String curso[] = {"Tec.Inf", "Direito", "Arquitetura"};
-    public String matricula[] = {"154", "355", "444"};
+import java.util.Scanner;
 
-    public void cadastroAlunos(String nome[]) {
-        for (int i = 0; i < nome.length; i++) {
-            System.out.println(nome[i]);
+public class Estudante {
+    Scanner in = new Scanner(System.in);
+
+    public String nome[] = {"Flávia","Roberto","Francisco"};
+    public int idade[] = {20, 50, 40};
+    public String curso[] = {"Ciencias da Computação", "Biologia", "Direito"};
+    public int matricula[] = {2020141, 303052, 408256};
+
+
+    public void cadEstudantes(String cadastro[]){
+        for (int i = 0; i < nome.length; i++){
+            String nomeA = nome[i];
+            int idadeA = idade[i];
+            String cursoA = curso[i];
+            int matriculaA = matricula[i];
+
+            System.out.printf("\nNome:%s\n Idade:%d\n Curso:%s\n Matricula:%s\n",nomeA ,idadeA ,cursoA ,matriculaA);
+
+        }
+    }
+    /*public void opcao(char escolha){
+        switch (escolha){
+            case 1:
+                escolherNome();
+                break;
+            case 2:
+                escolherIdade();
+                break;
+            case 3:
+                escolherCurso();
+            case 4:
+                escolherMatricula();
+                break;
         }
     }
 
-    public void editarAlunos() {
+    public void escolherNome() {
+        System.out.println("Informe outro nome: ");
+        String nomeAtual = this.nome;
+        this.nome = in.nextLine();
+        System.out.println("Nome alterado: " + this.nome);
 
     }
+
+    public void escolherIdade() {
+        System.out.println("Informe a idade: ");
+        int idadeAtual = this.idade;
+        this.idade = in.nextInt();
+        System.out.println("Nome alterado: " + this.idade);
+
+    }
+
+    public void escolherCurso() {
+        System.out.println("Informe o novo curso: ");
+        String cursoAtual = this.curso;
+        this.curso = in.nextLine();
+        System.out.println("Nome alterado: " + this.curso);
+
+    }
+
+    public void escolherMatricula() {
+        System.out.println("Informe a nova matricula: ");
+        int matriculaAtual = this.matricula;
+        this.matricula = in.nextInt();
+        System.out.println("Nome alterado: " + this.matricula);
+
+    }
+
+    public void imprimir() {
+        System.out.printf("Nome: %s\nIdade: %d\nCurso: %s\nMatrícula: %d\n-----\n", this.nome, this.idade, this.curso, this.matricula);
+    }*/
 }
