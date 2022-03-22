51e1f2e27135df76bca826f232a4ab6e6a2da594
Flavia Costa <flavinha.goncalves@gmail.com>
2022-03-22 20:09:41 -0300

    Ajuste impressão de dados. Imc

--- src/lista02_classesObjetos/entidades/Pessoa.java
+++ src/lista02_classesObjetos/entidades/Pessoa.java
@@ -7,23 +7,29 @@ public class Pessoa {
     public int peso;
     public String sexo;
 
-    public Double calculaImc(){
+    public Double calculaImc() {
         return peso / ((altura * altura));
     }
 
-    public void imprimeDados(){
-        System.out.println("Nome: " + this.nome);
-        System.out.println("Idade: " + this.idade);
-
-        if(calculaImc() <= 18.5){
+    public Double calculaImc(Double imc) {
+        if (calculaImc() <= 18.5) {
             System.out.print("Abaixo do peso normal\n");
-        }else if (calculaImc() > 18.5 && calculaImc() <= 25 ) {
+        } else if (calculaImc() > 18.5 && calculaImc() <= 25) {
             System.out.print("Peso Normal\n");
-        }else if(calculaImc() > 25 && calculaImc() <= 30){
+        } else if (calculaImc() > 25 && calculaImc() <= 30) {
             System.out.print("Acima do peso normal\n");
-        }else if (calculaImc() > 30){
+        } else if (calculaImc() > 30) {
             System.out.print("Obesidade\n");
         }
-        System.out.printf("Imc: %.2f" , this.calculaImc());
+        System.out.printf("Imc: %.2f", this.calculaImc());
+
+        return this.calculaImc();
+    }
+
+    public void imprimeDados() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.printf("Imc %.2f", this.calculaImc());
+
     }
 }
