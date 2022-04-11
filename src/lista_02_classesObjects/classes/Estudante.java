dafbd1fa6bc8ecba679805690e6ee46ea5bcbbdb
Flavia Costa <flavinha.goncalves@gmail.com>
2022-04-10 21:58:28 -0300

    Programa cadastro de Alunos. Construtores, private, getters and setters

--- src/lista_02_classesObjects/classes/Estudante.java
+++ src/lista_02_classesObjects/classes/Estudante.java
@@ -1,89 +1,121 @@
 package lista_02_classesObjects.classes;
 
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
-//Criada Classe private.
 
 public class Estudante {
-    public String nome;
-    public int idade;
-    public String curso;
-    public int matricula;
+    Scanner in = new Scanner(System.in);
+    private String nome;
+    private String curso;
+    private int idade;
+    private int matricula;
+
+    public Estudante(String nome) {
+        this.nome = nome;
+    }
+
+    public Estudante(String nome, String curso, int idade, int matricula) {
+        this.nome = nome;
+        this.curso = curso;
+        this.idade = idade;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
 
     public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
 
-    //Criado construtor de nome para auxilar na instanciação na main.
-    public Estudante(String nome) {
-        this.nome = nome;
-    }
+    public void selecioneAluno(int opcao){
+        if(opcao == 1){
 
-    //Construtor geral
-    public Estudante(String nome, int idade, String curso, int matricula) {
-        this.nome = nome;
-        this.idade = idade;
-        this.curso = curso;
-        this.matricula = matricula;
-    }
+        }else if(opcao == 2){
 
-    //Metodos para imprimir os dados individuais do estudante.
-
-    public void mostrarAluno() {System.out.printf("Novo nome: %s \n", this.nome);}
-    public void mostrarNovaIdade(){System.out.printf("Nova idade: %d \n", this.idade);}
-    public void mostrarNovoCurso() {System.out.printf("Novo curso: %s \n", this.curso);}
-    public void mostrarNovaMatricula(){System.out.printf("Nova matricula: %s \n", this.matricula);}
-    Scanner in = new Scanner(System.in);
-    public void opcao(int escolha){
-        if (escolha == 1){
-            System.out.println("O campo alterado foi NOME");
-            alteraNome();
-
-        }else if(escolha == 2){
-            System.out.println("O campo alterado foi IDADE");
-            alteraIdade();
-
-        }else if(escolha == 3){
-            System.out.println("O campo alterado foi CURSO");
-            alteraCurso();
-
-        }else if(escolha == 4){
-            System.out.println("O campo alterado foi MATRICULA");
-            alteraMatricula();
+        }else if(opcao == 3){
 
         }
     }
+    public void alterarCadastro(int opcao){
+        if(opcao == 1){
+            System.out.printf("Campo selecionado (Alterar Nome) ");
+            alterarNome();
+        }else if(opcao == 2){
+            System.out.printf("Campo selecionado (Alterar Curso)");
+            alterarCurso();
+        }else if(opcao == 3){
+            System.out.printf("Campo selecionado (Alterar Idade)");
+            alterarIdade();
+        }else if(opcao == 4){
+            System.out.printf("Campo selecionado (Alterar Matricula)...");
+            alterarMatricula();
+        }
+    }
 
-    //Metodos para guardar guardar o nome antigo e obter o novo.
-    public void alteraNome() {
-        String nomeAntigo = this.nome;
-        System.out.println("Informe um novo nome: ");
-        String novoNome = in.nextLine();
-        System.out.println("O nome antigo é " + nomeAntigo);
-        System.out.println("O novo nome é " + novoNome);
+    public void alterarNome(){
+        System.out.printf("Informe o novo nome: ");
+        String novoDado = in.nextLine();
+        String dadoAntigo = this.nome;
+        System.out.printf("=>Alterar Nome<=\n");
+        System.out.printf("Nome antigo: %s\nNovo nome: %s ", dadoAntigo, novoDado);
 
     }
-    public void alteraIdade() {
-        int idadeAntiga = this.idade;
-        System.out.println("Informe uma nova idade: ");
-        int novaIdade = in.nextInt();
-        System.out.println("A idade antiga é " + idadeAntiga);
-        System.out.println("A nova idade é " + novaIdade);
+    public void alterarCurso(){
+        System.out.printf("Informe o novo curso: ");
+        String novoDado = in.nextLine();
+        String dadoAntigo = this.curso;
+        System.out.printf("=>Alterar Curso<=\n");
+        System.out.printf("Curso antigo: %s\nNovo curso: %s ", dadoAntigo, novoDado);
 
     }
-    public void alteraCurso() {
-        String cursoAntigo = this.curso;
-        System.out.println("Informe um novo curso: ");
-        String novoCurso = in.nextLine();
-        System.out.println("O antigo curso é " + cursoAntigo);
-        System.out.println("O novo curso é " + novoCurso);
+    public void alterarIdade(){
+        System.out.printf("Informe o nova Idade: ");
+        int novoDado = in.nextInt();
+        int dadoAntigo = this.idade;
+        System.out.printf("=>Alterar Idade<=\n");
+        System.out.printf("Idade antiga: %d\nNova idade: %d ", dadoAntigo, novoDado);
 
     }
-    public void alteraMatricula() {
-        int antigaMatricula = this.matricula;
-        System.out.println("Informe uma nova matricula: ");
-        int novaMatricula = in.nextInt();
-        System.out.println("A idade antiga é " + antigaMatricula);
-        System.out.println("A nova matricula é " + novaMatricula);
+    public void alterarMatricula(){
+        System.out.printf("Informe o nova matricula: ");
+        int novoDado = in.nextInt();
+        int dadoAntigo = this.matricula;
+        System.out.printf("=>Alterar Matricula<=\n");
+        System.out.printf("Matricula antiga: %d\nNova matricula: %d ", dadoAntigo, novoDado);
 
+    }
+
+    @Override
+    public String toString() {
+        return  "Nome: " + nome + '\'' +
+                "\nCurso: " + curso + '\'' +
+                "\nIdade: " + idade +
+                "\nMatricula: " + matricula;
     }
 }
--- src/lista_02_classesObjects/program_cadastro_aluno.java
+++ src/lista_02_classesObjects/program_cadastro_aluno.java
@@ -2,7 +2,6 @@ package lista_02_classesObjects;
 
 import lista_02_classesObjects.classes.Estudante;
 
-import javax.sound.midi.Soundbank;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,51 +10,25 @@ public class program_cadastro_aluno {
         Locale.setDefault(Locale.US);
         Scanner in = new Scanner(System.in);
 
-        //Vetor atribuindo a quantidade de estudantes cadastrados
-        Estudante[] estudantes = new Estudante[3];
+        Estudante[] estudante = new Estudante[3];
+        estudante[0] = new Estudante("Renato","ADS",25,2003);
+        estudante[1] = new Estudante("Paty","ADS",29,2004);
+        estudante[2] = new Estudante("Flavia","CC",26,2005);
 
-        //Definição prévia dos dados cadastrais. Instanciação.
-        estudantes[0] = new Estudante("Flavia", 20, "CC", 202020);
-        estudantes[1] = new Estudante("José", 25, "ADS", 202022);
-        estudantes[2] = new Estudante("Maria", 28, "Direito", 202024);
-
-        //Laço for para ler o nome dos estudantes.
-        System.out.println("Lista de estudantes:");
-        for (int i = 0; i < estudantes.length; i++) {
-            System.out.println(estudantes[i).getNome());
+        System.out.printf("Alunos Matriculados\n");
+        for(int i = 0; i< estudante.length; i++){
+            System.out.println(i + 1 + "-" + estudante[i].getNome());
         }
 
-        //Interação com o usuário para obter a informação qual cadastro de aluno sofrerá alteração
-        System.out.println("Informe o aluno que deseja alterar o dado: ");
-        int escolha = in.nextInt();
+        System.out.printf("Informe qual aluno sofrerá alteração: ");
+        int selecioneAluno = in.nextInt();
+        Estudante alunoSelecionado = estudante[selecioneAluno - 1];
+        System.out.println(alunoSelecionado);
 
-        //Criação de uma variável para receber o nome de cada aluno
-        String nome = "";
-        for (int i = 0; i < estudantes.length; i++) {
-            if (escolha == 1) {
-                nome = estudantes[0].getNome();
-            } else if (escolha == 2) {
-                nome = estudantes[1].getNome();
-            } else if (escolha == 3) {
-                nome = estudantes[2].getNome();
-            }
-        }
-        //Instanciar o nome dos estudantes e mostrar na tela do usuário
-        Estudante nomeAluno = new Estudante(nome);
-        nomeAluno.mostrarAluno();
-
-        int idade, matricula; String curso;
-        for (int i = 0; i < estudantes.length; i++){
-            idade = estudantes[i].getIdade();
-            curso = estudantes[i].getCurso();
-            matricula = estudantes[i].getMatricula();
-        }
-        //Interação com usuário para definir qual campo será editado.
-        System.out.println("Informe o dado cadastral que deverá ser alterado: ");
-        System.out.printf("(1) - Nome\n(2) - Idade\n(3) - Curso\n(4) - Matricula\nDigite: ");
+        System.out.printf("Informe qual campo deseja alterar:\n1 - Nome \n2 - Curso \n3 - Idade \n4 - Matricula\n Digite:  ");
         int opcao = in.nextInt();
 
-        nomeAluno.opcao(opcao);
+        alunoSelecionado.alterarCadastro(opcao);
 
     }
 }
