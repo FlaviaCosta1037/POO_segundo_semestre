f08d9d762ad72f8c4cb3752e41ed96e6314582a2
Flavia Costa <flavinha.goncalves@gmail.com>
2022-05-11 22:21:56 -0300

    Criadas Classes clientes e contas abstratas.

--- src/lista07/Classes/ContaAbstrata.java
+++ src/lista07/Classes/ContaAbstrata.java
@@ -1,2 +1,48 @@
-package lista07.Classes;public class ContaAbstrata {
+package lista07.Classes;
+
+public abstract class ContaAbstrata {
+    private String numeroConta;
+    private double saldo;
+    private Cliente cliente;
+
+    abstract double obterLimite();
+
+    public ContaAbstrata(String numeroConta, double saldo, Cliente cliente) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double depositar(double valor){
+        return this.saldo += valor;
+    }
+
+    public double sacar(double valor){
+        return this.saldo -= valor;
+    }
+
 }
