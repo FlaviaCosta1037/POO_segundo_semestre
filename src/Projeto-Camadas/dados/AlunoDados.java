8138364945758e5a1582702ac71dd2acbbe2064b
Flavia Costa <flavinha.goncalves@gmail.com>
2022-05-19 21:42:44 -0300

    Inicializando projeto camadas

---
+++ src/Projeto-Camadas/dados/AlunoDados.java
@@ -0,0 +1,22 @@
+package dados;
+import entidades.Aluno;
+
+public class AlunoDados {
+	
+	private Aluno alunos [] = new Aluno[10];
+	
+	public void cadastrar(Aluno aluno) {
+
+		//Persistir aluno na base de dados, que neste exemplo é o array de alunos. 
+		
+	}
+	
+	public boolean alunoJaCadastrado(String cpf) {
+		
+		// Busca todos os alunos do array para verificar se o cpf já foi cadastrado.
+		
+		return false;
+		
+	}
+
+}
