6e117201931e2a70d2022d5bac96f3e8331cb3d2
Flavia Costa <flavinha.goncalves@gmail.com>
2022-05-21 18:31:36 -0300

    Criada Tipo de conta poupança simples.

--- src/lista07/Classes/Poupanca.java
+++ src/lista07/Classes/Poupanca.java
@@ -2,6 +2,12 @@ package lista07.Classes;
 
 public class Poupanca extends ContaAbstrata{
     public double saldo;
+
+    public Poupanca(String numeroConta, double saldo, Cliente cliente) {
+        super(numeroConta, saldo, cliente);
+        this.saldo = saldo;
+    }
+
     @Override
     public double obterLimite() {
         return getSaldo();
