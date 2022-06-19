f626f11bc92a3a7c36a9785f391aa4dddf5a210c
Flavia Costa <flavinha.goncalves@gmail.com>
2022-06-19 19:22:29 -0300

    Ordenação de idade implementando Iterface Comparable, método Set e sobrescrevendo o método compareTo.

---
+++ src/interfacesOrdenacao/Pessoa.java
@@ -0,0 +1,47 @@
+package interfacesOrdenacao;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    //Método já será atribuido na main para ordenação da idade
+    @Override
+
+    public int compareTo(Pessoa outraPessoa) {
+        if (this.getIdade() < outraPessoa.getIdade())
+            return -1;
+        if (this.getIdade() > outraPessoa.getIdade())
+            return 1;
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
+
---
+++ src/interfacesOrdenacao/programa1.java
@@ -0,0 +1,25 @@
+package interfacesOrdenacao;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class programa1 {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa("José", 20);
+        Pessoa p2 = new Pessoa("Maria", 40);
+        Pessoa p3 = new Pessoa("Pedro", 30);
+
+        Set<Pessoa> colecao = new TreeSet<Pessoa>();
+        colecao.add(p1);
+        colecao.add(p2);
+        colecao.add(p3);
+
+        //A lista será exibida por ordem de idade
+        for(Pessoa p: colecao){
+            System.out.printf("%d anos\n", p.getIdade());
+        }
+    }
+}
